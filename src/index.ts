e857d285e0f6c944ddf0a940f05e85910f0b688a
Youhan Wu <youhan.wu@outlook.com>
2022-07-17 00:57:17 -0500

    v1.0.3 : realize lazy loading

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,71 @@
 import { title2URL } from './redirectUrl';
-import 'dotenv/config';
 
-const observer = new MutationObserver(resetTimer);
-let timer = setTimeout(action, 2000, observer);
-observer.observe(document, { childList: true, subtree: true });
+let redirectUrl: undefined | null | string = undefined;
 
-// reset timer every time something changes
-function resetTimer(changes, observer) {
-  clearTimeout(timer);
-  timer = setTimeout(action, 500, observer);
-}
+const waitForRender = (...selectors: string[]) =>
+  new Promise<Element[]>((resolve, reject) => {
+    let totDelay = 0;
+    const delay = 500;
+    const f = () => {
+      totDelay++;
+      if (totDelay >= 200) reject('Timeout');
+      const elements = selectors.map((selector) =>
+        document.querySelector(selector)
+      );
+      if (elements.every((element) => element != null)) resolve(elements);
+      else setTimeout(f, delay);
+    };
+    f();
+  });
 
-function action(observer) {
-  observer.disconnect();
-  const parentBar = document.querySelector<HTMLDivElement>(
-    'div[class*="TabHeaderContainer"]'
-  );
-  const hostname = window.location.hostname;
-  const isUsSite = hostname === 'leetcode.com';
-  const newTab = parentBar?.lastChild.cloneNode(true);
-  const pagination = document.querySelector(
-    'div[class^="pagination-screen__"]'
-  );
-  const code = isUsSite
-    ? parseInt(pagination?.firstChild?.firstChild?.textContent)
-    : parseInt(pagination?.firstChild?.textContent);
-  (<Element>newTab)?.setAttribute('data-key', 'AcWing');
-  (<Element>newTab)?.setAttribute('data-cy', 'AcWing');
-  const url = title2URL.get(code);
-  if (url) {
-    (<HTMLAnchorElement>newTab?.firstChild)?.setAttribute('href', url);
-    (<HTMLAnchorElement>newTab?.firstChild)?.setAttribute('target', '_blank');
-  } else {
-    (<HTMLAnchorElement>newTab?.firstChild)?.setAttribute('href', '#');
-    (<HTMLAnchorElement>newTab?.firstChild)?.setAttribute(
-      'onclick',
-      "alert('Not found in AcWing')"
-    );
+const getURL = async (
+  isUs: boolean,
+  cnt: number
+): Promise<typeof redirectUrl> => {
+  try {
+    if (cnt >= 10) throw 'Timeout';
+    const [title] = isUs
+      ? await waitForRender('div[data-cy="question-title"]')
+      : await waitForRender('h4[data-cypress="QuestionTitle"]');
+    const code = isUs
+      ? ~~title?.firstChild?.textContent
+      : ~~title?.firstChild?.firstChild?.textContent;
+    if (code === 0) return await getURL(isUs, cnt + 1);
+    else return title2URL.get(code) ? title2URL.get(code) : '';
+  } catch (error) {
+    console.log(error, 'cannot get problem id.');
+    return null;
   }
-  (<SVGElement>(
-    newTab?.firstChild?.firstChild?.firstChild?.firstChild?.firstChild
-  ))?.remove();
-  // for US site
-  if (isUsSite)
-    (<HTMLSpanElement>(
-      newTab?.firstChild?.firstChild?.firstChild?.firstChild?.lastChild
-    ))!.innerText = 'AcWing';
-  // for CN site
-  else
-    (<HTMLDivElement>(
-      newTab?.firstChild?.firstChild?.firstChild?.firstChild
-    ))!.innerText = 'AcWing';
-  parentBar?.appendChild(newTab);
-}
+};
+
+(async () => {
+  try {
+    const [parentBar] = await waitForRender('div[class*="TabHeaderContainer"]');
+    const newTab = <Element>parentBar?.lastChild?.cloneNode(true);
+    newTab?.setAttribute('data-key', 'AcWing');
+    newTab?.setAttribute('data-cy', 'AcWing');
+    (<SVGElement>(
+      newTab?.firstChild?.firstChild?.firstChild?.firstChild?.firstChild
+    ))?.remove();
+    const isUsSite = window.location.hostname === 'leetcode.com';
+    isUsSite
+      ? ((<HTMLSpanElement>(
+          newTab?.firstChild?.firstChild?.firstChild?.firstChild?.lastChild
+        ))!.innerText = 'AcWing')
+      : ((<HTMLDivElement>(
+          newTab?.firstChild?.firstChild?.firstChild?.firstChild
+        ))!.innerText = 'AcWing');
+    (<HTMLAnchorElement>newTab?.firstChild).replaceWith(
+      <HTMLDivElement>newTab?.firstChild?.firstChild
+    );
+    newTab.addEventListener('click', async () => {
+      if (redirectUrl === undefined) redirectUrl = await getURL(isUsSite, 0);
+      if (redirectUrl === '') window.alert('Not found in AcWing.');
+      else if (redirectUrl === null) window.alert('Script loading failed.');
+      else window.open(redirectUrl, '_blank');
+    });
+    parentBar?.appendChild(newTab);
+  } catch (error) {
+    console.error(error, 'script leetcode2acwing did not load.');
+  }
+})();
