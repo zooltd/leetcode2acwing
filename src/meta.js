74b9db1f3f977113f9f75e6d515c2f21e38ccbe8
Youhan Wu <youhan.wu@outlook.com>
2022-07-17 01:03:35 -0500

    v1.0.3 : update metadata

--- src/meta.js
+++ src/meta.js
@@ -1,14 +1,17 @@
 // ==UserScript==
-// @name        leetcode2acwing
-// @namespace   Violentmonkey Scripts
-// @description Redirect LeetCode problem site to Acwing solution site.
-// @match       https://leetcode.com/problems/*
-// @match       https://leetcode.cn/problems/*
-// @grant       GM_addStyle
-// @version     process.env.VERSION
-// @author      process.env.AUTHOR
-// @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@2,npm/@violentmonkey/ui@0.7
-// @run-at      document-idle
+// @name                leetcode2acwing
+// @name:zh-CN          leetcode2acwing
+// @namespace           zooltd
+// @description         Redirect LeetCode problem site to Acwing solution site.
+// @description:zh-CN   从LeetCode问题页面跳转至AcWing题解页面。
+// @match               https://leetcode.com/problems/*
+// @match               https://leetcode.cn/problems/*
+// @grant               GM_addStyle
+// @version             process.env.VERSION
+// @author              process.env.AUTHOR
+// @require             https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@2,npm/@violentmonkey/ui@0.7
+// @run-at              document-idle
+// @license             MIT
 // ==/UserScript==
 
 /**
