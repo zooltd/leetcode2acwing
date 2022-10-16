fec533619d19ebcf63e0e1c6e0e07014631b1f0b
Youhan Wu <youhan.wu@outlook.com>
2022-10-15 19:36:57 -0500

    v1.0.7 : update metadata

--- src/meta.js
+++ src/meta.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name                leetcode2acwing
 // @name:zh-CN          leetcode2acwing
-// @namespace           Violentmonkey Scripts
+// @namespace           https://github.com/zooltd/leetcode2acwing
+// @supportURL          https://github.com/zooltd/leetcode2acwing
+// @home-url            https://greasyfork.org/en/scripts/447992-leetcode2acwing
 // @description         Redirect LeetCode problem site to Acwing solution site.
 // @description:zh-CN   从LeetCode问题页面跳转至AcWing题解页面。
 // @match               https://leetcode.com/problems/*
